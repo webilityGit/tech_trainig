2638a802b83f43d1a393f9e4ca4f0e920444e508
web_odoo <vladimir@webility.rs>
2024-06-11 14:06:16 +0000

    [ADD] New Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Registration of Motorcycle',
+    'description': 'Motorcycle Registry',
+    'author': 'webilityGit',
+    'version':'0.0.1',
+    'category':'Kawiil/Custom Modules',
+    'website': 'https://github.com/webilityGit/tech_training',
+    'application': True,
+}
