8cd52a9c03c792bf5b9b651fd016ebf4396e991c
web_odoo <vladimir@webility.rs>
2024-07-01 08:09:37 +0000

    [ADD] New Module

--- motorcycle_website/__manifest__.py
+++ motorcycle_website/__manifest__.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-{
-    'name': "Motorcycle Website",
-    'author': 'webilityGit',
-    'website': 'https://github.com/webilityGit/tech_training',
-    'category': 'Kawiil/Custom Modules',
-    'version': '0.0.1',
-    'depends': ['motorcycle_stock, website'],
-    'data': [
-        'views/motorcycle_report_template.xml',
-        'views/motorcycle_report_views.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-       # 'demo/demo.xml',
-    ],
-    'application' : True,
-}
+#{
+#    'name': "Motorcycle Website",
+#    'author': 'webilityGit',
+#    'website': 'https://github.com/webilityGit/tech_training',
+#    'category': 'Kawiil/Custom Modules',
+#    'version': '0.0.1',
+#    'depends': ['motorcycle_stock, website'],
+#    'data': [
+#        'views/motorcycle_report_template.xml',
+#        'views/motorcycle_report_views.xml',
+#    ],
+#    # only loaded in demonstration mode
+#    'demo': [
+#       # 'demo/demo.xml',
+#    ],
+#    'application' : True,
+#}
